0e038889b2fa83342921aae585d68b795d723873
Branden Lee Esses <branden.esses@colorado.edu>
2023-11-14 16:33:58 -0700

    Plot format changes for APE fitting

--- ape/ape.py
+++ ape/ape.py
@@ -54,7 +54,7 @@ def ape_plotting():
         return f"Error selecting the y axis data. Did you mean to pick index {data_column}?"
 
     x_range = np.linspace(np.min(wavelength), np.max(wavelength), 100)
-    plot_data.append(go.Scatter(x=wavelength, y=selected_data, mode='markers', name=r'$\textrm{Column ' + str(data_column) + '}$'))
+    plot_data.append(go.Scatter(x=wavelength, y=selected_data, mode='markers', name=r'$\small\textrm{Col. ' + str(data_column) + '}$'))
 
     popt2, pcov2 = curve_fit(func2, wavelength, selected_data, p0=None)
     popt3, pcov3 = curve_fit(func3, wavelength, selected_data, p0=None)
@@ -68,7 +68,7 @@ def ape_plotting():
     r_squared2 = 1 - (ss_res2 / ss_tot2)
     # print(r_squared2)
     y_pred2 = func2(x_range, popt2[0], popt2[1])
-    plot_data.append(go.Scatter(x=x_range, y=y_pred2, mode='lines', name=r"$x$"))
+    plot_data.append(go.Scatter(x=x_range, y=y_pred2, mode='lines', name=r"$\small x$"))
 
     # 3 parameters
     residuals3 = selected_data - func3(wavelength, *popt3)
@@ -76,7 +76,7 @@ def ape_plotting():
     ss_tot3 = np.sum((selected_data - np.mean(selected_data)) ** 2)
     r_squared3 = 1 - (ss_res3 / ss_tot3)
     y_pred3 = func3(x_range, popt3[0], popt3[1], popt3[2])
-    plot_data.append(go.Scatter(x=x_range, y=y_pred3, mode='lines', name=r"$x^2$"))
+    plot_data.append(go.Scatter(x=x_range, y=y_pred3, mode='lines', name=r"\small $x^2$"))
     # print(r_squared3)
 
     # 4 parameters
@@ -85,7 +85,7 @@ def ape_plotting():
     ss_tot4 = np.sum((selected_data - np.mean(selected_data)) ** 2)
     r_squared4 = 1 - (ss_res4 / ss_tot4)
     y_pred4 = func4(x_range, popt4[0], popt4[1], popt4[2], popt4[3])
-    plot_data.append(go.Scatter(x=x_range, y=y_pred4, mode='lines', name=r"$x^3$"))
+    plot_data.append(go.Scatter(x=x_range, y=y_pred4, mode='lines', name=r"$\small x^3$"))
     # print(r_squared3)
 
     # 5 parameters
@@ -94,19 +94,19 @@ def ape_plotting():
     ss_tot5 = np.sum((selected_data - np.mean(selected_data)) ** 2)
     r_squared5 = 1 - (ss_res5 / ss_tot5)
     y_pred5 = func5(x_range, popt5[0], popt5[1], popt5[2], popt5[3], popt5[4])
-    plot_data.append(go.Scatter(x=x_range, y=y_pred5, mode='lines', name=r"$x^4$"))
+    plot_data.append(go.Scatter(x=x_range, y=y_pred5, mode='lines', name=r"$\small x^4$"))
     # print(r_squared3)
 
     layout = go.Layout(title=r'$\Large\textrm{Fitting}$',
-                       yaxis={'title': r'$\textrm{Column ' + str(data_column) + '}$', 'automargin': True, 'tickformat':'none'},
-                       xaxis={'title': r'$\textrm{Wavelength (cm}^{-1}\textrm{)}$', 'automargin': True, 'tickformat':'none'},
+                       yaxis={'title': r'$\small\textrm{Column ' + str(data_column) + '}$', 'automargin': True, 'tickformat':'none'},
+                       xaxis={'title': r'$\small\textrm{Wavelength (cm}^{-1}\textrm{)}$', 'automargin': True, 'tickformat':'none'},
                        template="none", height=500, width=666.67)
 
     figure = go.Figure(data=plot_data, layout=layout)
     figure.update_layout(
         font=dict(
             family="Arial",
-            size=22,  # Set the font size here
+            size=18,  # Set the font size here
             color="Black"
         )
     )
@@ -116,7 +116,7 @@ def ape_plotting():
     A5, B5, C5, D5, E5 = popt5
     plot_div = offline.plot(figure, auto_open=False, output_type='div')
     table_layout = go.Layout(title=r'$\textrm{Fit function: }A + Bx + Cx^2 + Dx^3 + Ex^4$')
-    table = go.Figure(data=[go.Table(header=dict(values=['Order',r'$A$', r'$B$', r'$C$', r'$D$', r'$E$', r'$R^2$']),
+    table = go.Figure(data=[go.Table(header=dict(values=['Order','A', 'B', 'C', 'D', 'E', 'R^2']),
                                    cells=dict(values=[['Linear','Quadratic','Cubic','Quartic'],[A2,A3,A4,A5], [B2,B3,B4,B5], ['-',C3,C4,C5], ['-','-',D4,D5],['-','-','-',E5],[r_squared2,r_squared3,r_squared4,r_squared5]]))], layout=table_layout)
     table_div = offline.plot(table, auto_open=False, output_type='div')
     return render_template("ape.html",title="APE Fitting", figures=True, plot_div=plot_div, table_div=table_div, data_column=data_column, x_axis=x_axis, header=header)
